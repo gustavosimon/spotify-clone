b13d21307024fd5b7d8aeca8d04678c810ae9b61
gustavosimon <gustavo.andre.simon@hotmail.com>
2020-03-28 00:13:25 -0300

    Add playlist section

--- desktop/src/components/Playlist/index.js
+++ desktop/src/components/Playlist/index.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container } from './styles';
+import { Container, Ul, Span, Li } from './styles';
+
+const data = [
+    'Funk Hits',
+    'Eletronicas 2020',
+    'Bomba Patch',
+    'Rock 80-90 internacional',
+];
+
+const library = [
+    'Feito para você',
+    'Tocadas recentemente',
+    'Músicas Curtidas',
+    'Álbuns',
+    'Artistas',
+    'Podcasts',
+];
 
 export default function Playlist() {
-    return <Container>skjhfdslkjhfsjdhflkjdshk</Container>;
+    const [selected, setSelected] = useState('');
+    return (
+        <Container>
+            <Span>Sua biblioteca</Span>
+            <Ul>
+                {library.map(d => (
+                    <Li selected={selected === d}>
+                        <button onClick={() => setSelected(d)}>{d}</button>
+                    </Li>
+                ))}
+            </Ul>
+            <Span>Playlists</Span>
+            <Ul>
+                {data.map(d => (
+                    <Li selected={selected === d}>
+                        <button onClick={() => setSelected(d)}>{d}</button>
+                    </Li>
+                ))}
+            </Ul>
+        </Container>
+    );
 }
--- desktop/src/styles.js
+++ desktop/src/styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import 'perfect-scrollbar-react/dist/style.min.css';
 
 export const Container = styled.div`
     height: 100%;
