e1c138fb4f16a3063bc30dbbd344532737965dfc
MarceloHBoff <marceloboff20@gmail.com>
2020-04-05 21:13:58 -0300

    Add public route for music

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,31 +1,35 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import routes from './routes';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import path from "path";
+import routes from "./routes";
 
 class App {
-    constructor() {
-        this.server = express();
+  constructor() {
+    this.server = express();
 
-        mongoose.connect('mongodb://localhost:27017/spotify', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: true,
-        });
+    mongoose.connect("mongodb://localhost:27017/spotify", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true
+    });
 
-        this.middlewares();
-        this.routes();
-    }
+    this.middlewares();
+    this.routes();
+  }
 
-    middlewares() {
-        this.server.use(express.json());
-        this.server.use(cors());
-        this.server.use(express.static('public'));
-    }
+  middlewares() {
+    this.server.use(express.json());
+    this.server.use(cors());
+    this.server.use(
+      "/public",
+      express.static(path.resolve(__dirname, "..", "public"))
+    );
+  }
 
-    routes() {
-        this.server.use(routes);
-    }
+  routes() {
+    this.server.use(routes);
+  }
 }
 
 export default new App().server;
