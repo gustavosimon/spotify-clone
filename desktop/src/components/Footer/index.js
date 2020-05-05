7ccd4e75236ac1596308966b4b1ebced20c2eb5b
gustavosimon <gustavo.andre.simon@hotmail.com>
2020-05-04 22:25:07 -0300

    Add the music slider bar

--- desktop/src/components/Footer/index.js
+++ desktop/src/components/Footer/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   MdPlayCircleOutline,
   MdPauseCircleOutline,
@@ -8,6 +8,7 @@ import {
 
 import disc from '../../assets/disc.png';
 import ReactPlayer from 'react-player';
+import ReactInputSlider from 'react-input-slider';
 
 import { useMusic } from '../../hooks/music';
 
@@ -15,6 +16,8 @@ import { Container, Wrapper, Music } from './styles';
 
 export default function Footer() {
   const { play, setPlay, music, setMusic } = useMusic();
+
+  const [progress, setProgress] = useState({});
 
   function handlePlayMusic(music) {
     setPlay(true);
@@ -31,25 +34,51 @@ export default function Footer() {
         </div>
       </Music>
       <Wrapper>
-        <button type="button">
-          <MdSkipPrevious size={26} color="#fff" />
-        </button>
-        {play ? (
-          <button type="button" onClick={() => setPlay(false)}>
-            <MdPauseCircleOutline size={32} color="#fff" />
+        <div>
+          <button type="button">
+            <MdSkipPrevious size={26} color="#fff" />
           </button>
-        ) : (
-          <button type="button" onClick={() => handlePlayMusic(music)}>
-            <MdPlayCircleOutline size={32} color="#fff" />
+          {play ? (
+            <button type="button" onClick={() => setPlay(false)}>
+              <MdPauseCircleOutline size={32} color="#fff" />
+            </button>
+          ) : (
+            <button type="button" onClick={() => handlePlayMusic(music)}>
+              <MdPlayCircleOutline size={32} color="#fff" />
+            </button>
+          )}
+          <button type="button">
+            <MdSkipNext size={26} color="#fff" />
           </button>
-        )}
-        <button type="button">
-          <MdSkipNext size={26} color="#fff" />
-        </button>
-        <ReactPlayer
+        </div>
+        <ReactInputSlider
+          styles={{
+            active: {
+              backgroundColor: '#B3B3B3',
+              height: 5,
+              '&:hover': {
+                backgroundColor: 'rgba(30, 215, 96)',
+              },
+            },
+            track: {
+              backgroundColor: '#404040',
+              height: 5,
+              width: 650,
+            },
+            thumb: {
+              width: 10,
+              height: 10,
+              opacity: 0,
+            },
+          }}
+          xmax={progress.loadedSeconds}
+          x={progress.playedSeconds}
+        />
+        {/* <ReactPlayer
           url="http://localhost:3333/public/Vem_Me_Satisfazer.mp3"
           playing={play}
-        />
+          onProgress={setProgress}
+        /> */}
       </Wrapper>
       <div>final</div>
     </Container>
--- desktop/src/components/Footer/styles.js
+++ desktop/src/components/Footer/styles.js
@@ -1,37 +1,38 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    height: 90px;
-    background: #282828;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  height: 90px;
+  background: #282828;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const Wrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `;
 
 export const Music = styled.div`
-    span {
-        display: block;
-        margin-bottom: 5px;
-    }
-    padding: 20px;
-    color: #fff;
-    font-weight: bold;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    span + span {
-        font-size: 10px;
-        color: #ccc;
-    }
-    img {
-        width: 50px;
-        height: 50px;
-        margin-right: 8px;
-    }
+  span {
+    display: block;
+    margin-bottom: 5px;
+  }
+  padding: 20px;
+  color: #fff;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  span + span {
+    font-size: 10px;
+    color: #ccc;
+  }
+  img {
+    width: 50px;
+    height: 50px;
+    margin-right: 8px;
+  }
 `;
